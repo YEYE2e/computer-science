38d6c79f736bf15d3a6158744b30d747abefafce
Yan Portero <yanweiporteroguzman@gmail.com>
2026-01-27 23:15:08 -0500

    Update structure of progress

---
+++ Progress/IntroCS/Introduction to CS and Programming using Python/Lecture 2/ejercicios.py
@@ -0,0 +1,9 @@
+# usertext = input("ingrese:")
+# print("I can "+usertext+" better tham you")
+# usertext2 = (usertext+" ")
+# print (usertext2*5)
+# print(f"{usertext}hola")
+# 
+num = 5
+numR = int(input("Lookup number:"))
+print(5==numR)
