99f57f617c634ea902f77f61b2753c17fd55c008
Yan Portero <yanweiporteroguzman@gmail.com>
2026-02-09 05:03:44 -0500

    make s3

---
+++ Progress/IntroCS/Introduction to CS and Programming using Python/Lecture 3/file.py
@@ -0,0 +1,4 @@
+#Assume you are given a positive integer variable named N. Write a piece of Python code that prints hello world on separate lines, N times. You can use either a while loop or a for loop.
+n = int(input("Ingrese el valor"))
+for i in range(n):
+    print('Hello world')
